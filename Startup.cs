058b1445cb5ea8b58ae96402418127e6d3d991d5
Khang Nguyen <bcgaming168@gmail.com>
2022-06-03 01:30:57 +0700

    cart

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,6 @@
 using AspNetCoreHero.ToastNotification;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.FileProviders;
 using System.Text.Encodings.Web;
 using System.Text.Unicode;
 
@@ -50,6 +52,77 @@ namespace ShopManagementMVCConsume
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
+
+
+
+
+            //app.UseDefaultFiles();
+            //app.UseFileServer();
+            //StaticFileOptions options = new StaticFileOptions();
+            //FileExtensionContentTypeProvider contentTypeProvider = (FileExtensionContentTypeProvider)options.ContentTypeProvider ?? new FileExtensionContentTypeProvider();
+            //contentTypeProvider.Mappings.Add(".unityweb", "application/octet—stream");
+            //contentTypeProvider.Mappings.Add(".br", "application/octet—stream");
+            //options.ContentTypeProvider = contentTypeProvider; app.UseStaticFiles(options);
+
+            app.UseFileServer();
+            StaticFileOptions option = new StaticFileOptions();
+            FileExtensionContentTypeProvider contentTypeProvider = (FileExtensionContentTypeProvider)option.ContentTypeProvider ??
+            new FileExtensionContentTypeProvider();
+            contentTypeProvider.Mappings.Remove(".unityweb");
+            contentTypeProvider.Mappings.Add(".unityweb", "application/unityweb");
+            // Datafile mappings
+            contentTypeProvider.Mappings.Add(".data", "application/octet-stream");
+            contentTypeProvider.Mappings.Add(".data.gz", "application/octet-stream");
+            contentTypeProvider.Mappings.Add(".data.br", "application/octet-stream");
+            // js filemappings
+            contentTypeProvider.Mappings.Add(".js.gz", "application/javascript");
+            contentTypeProvider.Mappings.Add(".js.br", "application/javascript");
+            //wasm file mappings
+            contentTypeProvider.Mappings.Remove(".wasm");
+            contentTypeProvider.Mappings.Add(".wasm", "application/wasm");
+            contentTypeProvider.Mappings.Add(".wasm.gz", "application/wasm");
+            contentTypeProvider.Mappings.Add(".wasm.br", "application/wasm");
+
+            option.ContentTypeProvider = contentTypeProvider;
+            option.OnPrepareResponse = context =>
+            {
+                IHeaderDictionary headers = context.Context.Response.Headers;
+                string contentType = headers["Content-Type"];
+
+                if (context.File.Name.EndsWith("js.br"))
+                {
+                    contentType = "application/javascript";
+                    headers.Add("Content-Encoding", "br");
+                }
+                else if (context.File.Name.EndsWith("data.br"))
+                {
+                    contentType = "application/octet-stream";
+                    headers.Add("Content-Encoding", "br");
+                }
+                else if (context.File.Name.EndsWith("wasm.br"))
+                {
+                    contentType = "application/wasm";
+                    headers.Add("Content-Encoding", "br");
+                }
+
+                headers["Content-Type"] = contentType;
+
+            };
+            app.UseStaticFiles(option);
+
+            var provider = new FileExtensionContentTypeProvider();
+            // Add new mappings
+            provider.Mappings[".unityweb"] = "application/unityweb";
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+            Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")),
+                //RequestPath = "/StaticContentDir",
+                ContentTypeProvider = provider
+            });
+
         }
     }
 }
